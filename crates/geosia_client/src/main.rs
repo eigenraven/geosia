7c4d5b2b33974be0d2c7d024cc6e6b315bfb3d60
Raven Szewczyk <git@eigenraven.me>
2023-06-25 20:10:44 +0100

    Update to bevy 0.11 snapshot

--- crates/geosia_client/src/main.rs
+++ crates/geosia_client/src/main.rs
@@ -23,16 +23,16 @@ fn main() {
 
     let mut app = App::new();
     // Bevy Base
-    app.add_plugin(LogPlugin::default())
-        .add_plugin(TaskPoolPlugin::default())
-        .add_plugin(TypeRegistrationPlugin::default())
-        .add_plugin(FrameCountPlugin::default())
-        .add_plugin(TimePlugin::default())
-        .add_plugin(TransformPlugin::default())
-        .add_plugin(HierarchyPlugin::default())
-        .add_plugin(DiagnosticsPlugin::default())
-        .add_plugin(InputPlugin::default())
-        .add_plugin(WindowPlugin {
+    app.add_plugins(LogPlugin::default())
+        .add_plugins(TaskPoolPlugin::default())
+        .add_plugins(TypeRegistrationPlugin::default())
+        .add_plugins(FrameCountPlugin::default())
+        .add_plugins(TimePlugin::default())
+        .add_plugins(TransformPlugin::default())
+        .add_plugins(HierarchyPlugin::default())
+        .add_plugins(DiagnosticsPlugin::default())
+        .add_plugins(InputPlugin::default())
+        .add_plugins(WindowPlugin {
             primary_window: Some(Window {
                 title: "Geosia".to_string(),
                 present_mode: PresentMode::AutoNoVsync,
@@ -41,24 +41,24 @@ fn main() {
             exit_condition: ExitCondition::OnPrimaryClosed,
             close_when_requested: true,
         })
-        .add_plugin(AccessibilityPlugin)
-        .add_plugin(AssetPlugin::default())
-        .add_plugin(ScenePlugin::default())
-        .add_plugin(WinitPlugin::default())
-        .add_plugin(RenderPlugin::default())
-        .add_plugin(ImagePlugin::default())
-        .add_plugin(PipelinedRenderingPlugin::default())
-        .add_plugin(CorePipelinePlugin::default())
-        .add_plugin(SpritePlugin::default())
-        .add_plugin(TextPlugin::default())
-        .add_plugin(UiPlugin::default())
-        .add_plugin(PbrPlugin::default())
-        .add_plugin(AudioPlugin::default())
-        .add_plugin(GilrsPlugin::default())
-        .add_plugin(AnimationPlugin::default())
-        .add_plugin(GltfPlugin::default());
+        .add_plugins(AccessibilityPlugin)
+        .add_plugins(AssetPlugin::default())
+        .add_plugins(ScenePlugin::default())
+        .add_plugins(WinitPlugin::default())
+        .add_plugins(RenderPlugin::default())
+        .add_plugins(ImagePlugin::default())
+        .add_plugins(PipelinedRenderingPlugin::default())
+        .add_plugins(CorePipelinePlugin::default())
+        .add_plugins(SpritePlugin::default())
+        .add_plugins(TextPlugin::default())
+        .add_plugins(UiPlugin::default())
+        .add_plugins(PbrPlugin::default())
+        .add_plugins(AudioPlugin::default())
+        .add_plugins(GilrsPlugin::default())
+        .add_plugins(AnimationPlugin::default())
+        .add_plugins(GltfPlugin::default());
 
-    app.add_plugin(debug_window::DebugWindow);
+    app.add_plugins(debug_window::DebugWindow);
 
     app.run();
 }
@@ -71,7 +71,7 @@ mod debug_window {
 
     impl Plugin for DebugWindow {
         fn build(&self, app: &mut App) {
-            app.add_startup_system(debug_window_setup);
+            app.add_systems(Startup, debug_window_setup);
         }
     }
 
@@ -82,7 +82,8 @@ mod debug_window {
         commands
             .spawn(NodeBundle {
                 style: Style {
-                    size: Size::new(Val::Percent(50.0), Val::Percent(50.0)),
+                    width: Val::Percent(50.0),
+                    height: Val::Percent(50.0),
                     align_items: AlignItems::Center,
                     justify_content: JustifyContent::Center,
                     ..default()
